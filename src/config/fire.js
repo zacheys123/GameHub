0f5268033dc316382864f0d35025535c1ec443e3
zacheyshop@gmail.com <12keysjams@gmail.com>
2022-09-07 16:23:41 +0300

    first game commit

--- src/config/fire.js
+++ src/config/fire.js
@@ -1,7 +1,7 @@
 import 'firebase/firestore';
 import firebase from 'firebase/compat/app';
 const app = firebase.initializeApp({
-	apiKey: 'AIzaSyCtVfyfUgcWxCcGCdJK_NiZ8B67JLdjAA4',
+	apiKey: REACT_APP_firebase_key,
 	authDomain: process.env.REACT_APP_authDomain,
 	projectId: process.env.REACT_APP_projectId,
 	storageBucket: process.env.REACT_APP_storageBucket,
